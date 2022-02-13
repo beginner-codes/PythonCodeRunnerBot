4673e5fe6c5bc4be5db5b2667daa59a548ea0415
Zech Zimmerman <hi@zech.codes>
2022-02-13 11:45:36 -0500

    Simplify code runner and update to use new structure

--- beginner/cogs/code_runner.py
+++ beginner/cogs/code_runner.py
@@ -1,13 +1,9 @@
 from beginner.cog import Cog
-from beginner.colors import *
-from beginner.brainfuck_runner import BrainfuckInterpreter
 from datetime import datetime, timedelta
+from nextcord.ext.commands import command
 from typing import Tuple
 import asyncio
-import black
-import dis
 import nextcord
-import io
 import json
 import pathlib
 import re
@@ -18,43 +14,10 @@ class CodeRunner(Cog):
         super().__init__(client)
         self._exec_rate_limit = {}
         self._code_runner_emojis = "▶️⏯"
-        self._formatting_emojis = "✏️📝"
 
-    @Cog.command()
-    async def dis(self, ctx, *, content=""):
-        source = re.match(
-            r"^(?:```(?:py|python)\n)?\n?(.+?)(?:\n```)?$", content, re.DOTALL
-        ).groups()[0]
-        buffer = io.StringIO()
-        try:
-            code = compile(source, "<discord>", "exec")
-        except SyntaxError as excp:
-            msg, (file, line_no, column, line) = excp.args
-            spaces = " " * (column - 1)
-            await ctx.send(
-                embed=nextcord.Embed(
-                    title="Disassemble - Exception",
-                    description=f"```py\nLine {line_no}\n{line.rstrip()}\n{spaces}^\nSyntaxError: {msg}\n```",
-                    color=YELLOW,
-                )
-            )
-            return
-
-        dis.dis(code, file=buffer)
-        await ctx.send(
-            embed=nextcord.Embed(
-                title="Disassemble - Byte Code Instructions",
-                description=f"```asm\n{buffer.getvalue()}\n```",
-                color=BLUE,
-            )
-        )
-
-    @Cog.command()
+    @command()
     async def exec(self, ctx, *, content=""):
-        if content.strip().startswith("```bf") or content.strip().startswith(
-            "```brainfuck"
-        ):
-            await self._exec_brainfuck(ctx.message, content)
+        if not ctx.guild:
             return
 
         if content.strip() == "modules":
@@ -74,7 +37,7 @@ class CodeRunner(Cog):
                 embed=nextcord.Embed(
                     description=f"Here are all of the allowed modules:\n```\n{allowed_modules}\n```",
                     title="✅ Exec/Eval Allowed Modules",
-                    color=BLUE,
+                    color=0x0000FF,
                 )
             )
             return
@@ -91,6 +54,7 @@ class CodeRunner(Cog):
             return
 
         await self._exec(ctx.message, content, ctx.author)
+        self.log.debug(f"{ctx.author} ran code from {message.jump_url}")
 
     @Cog.listener()
     async def on_raw_reaction_add(self, reaction: nextcord.RawReactionActionEvent):
@@ -108,19 +72,21 @@ class CodeRunner(Cog):
             await message.remove_reaction(reaction.emoji, reaction.member)
             return
 
-        member = self.server.get_member(reaction.user_id)
+        member = channel.guild.get_member(reaction.user_id)
         if member.bot:
             return
 
         self._exec_rate_limit[reaction.message_id] = now
 
-        if reaction.emoji.name in self._code_runner_emojis and self.settings.get(
-            "EXEC_ENABLED", False
-        ):
-            await self._exec(message, message.content, reaction.member)
-
-        elif reaction.emoji.name in self._formatting_emojis:
-            await self._black_formatting(message, message.content, reaction.member)
+        if reaction.emoji.name in self._code_runner_emojis:
+            await self._exec(
+                message,
+                message.content,
+                reaction.member,
+            )
+            self.log.debug(
+                f"{reaction.member} used an emoji to run code in {message.jump_url}"
+            )
 
     async def _exec(
         self,
@@ -142,7 +108,7 @@ class CodeRunner(Cog):
                         "\n**NO PYTHON CODE BLOCK FOUND**\n\nThe command format is as follows:\n\n"
                         "\n!exec \\`\\`\\`py\nYOUR CODE HERE\n\\`\\`\\`\n"
                     ),
-                    color=RED,
+                    color=0xFF0000,
                 ),
                 reference=message,
                 allowed_mentions=nextcord.AllowedMentions(
@@ -152,7 +118,7 @@ class CodeRunner(Cog):
             return
 
         title = "✅ Exec - Success"
-        color = BLUE
+        color = 0x0000FF
 
         restricted = not member.guild_permissions.manage_messages
         if not restricted:
@@ -177,7 +143,7 @@ class CodeRunner(Cog):
         output = [out]
         if err:
             title = "❌ Exec - Exception Raised"
-            color = YELLOW
+            color = 0xFFFF00
             output.append(err)
 
         elif not out:
@@ -203,6 +169,7 @@ class CodeRunner(Cog):
         ).set_footer(text=f"!exec modules | Completed in {duration:0.4f} milliseconds")
         if "https://xkcd.com/353/" in old_out:
             embed.set_image(url="https://imgs.xkcd.com/comics/python.png")
+
         await message.channel.send(
             content="" if member is None else member.mention,
             embed=embed,
@@ -211,78 +178,6 @@ class CodeRunner(Cog):
                 replied_user=member is None,
                 users=[member] if member else False,
                 roles=[],
-            ),
-        )
-
-    async def _exec_brainfuck(self, message: nextcord.Message, content: str):
-        title = "✅ Exec Brainfuck - Success"
-        color = BLUE
-
-        code, user_input = re.match(
-            r"^.*?```(?:bf|brainfuck)?\s*(.+?)\s*```\s*(.+)?$", content, re.DOTALL
-        ).groups()
-
-        interpreter = BrainfuckInterpreter(
-            code, user_input + "\n" if user_input else ""
-        )
-        out, err = interpreter.run()
-
-        output = [out]
-        if err:
-            title = "❌ Exec - Exception Raised"
-            color = YELLOW
-            output.append(err)
-
-        elif not out:
-            output = ["*No output or exceptions*"]
-
-        out = "\n\n".join(output)
-        if len(out) > 1000:
-            out = out[:497] + "\n.\n.\n.\n" + out[-504:]
-        await message.channel.send(
-            embed=nextcord.Embed(
-                title=title, description=f"```\n{out}\n```", color=color
-            ),
-            reference=message,
-            allowed_mentions=nextcord.AllowedMentions(replied_user=True),
-        )
-
-    async def _black_formatting(
-        self, message: nextcord.Message, content: str, member: nextcord.Member = None
-    ):
-        if (
-            not len(content.strip())
-            or content.find("```") < 0
-            or content.rfind("```") <= 0
-        ):
-            code = content
-        else:
-            code = re.match(
-                r"^.*?```(?:py|python)?\s*(.+?)\s*```\s*$", content, re.DOTALL
-            ).group(1)
-
-        title = "✅ Formatting - Success"
-        color = BLUE
-
-        try:
-            formatted_code = f"py\n{black.format_file_contents(code, mode=black.FileMode(), fast=True)}\n"
-        except black.NothingChanged:
-            title = "✅ Formatting - Nothing Changed"
-            color = BLUE
-            formatted_code = "Code already formatted correctly."
-        except black.InvalidInput as e:
-            title = "❌ Formatting - Invalid Input"
-            color = YELLOW
-            formatted_code = f"\n{e}\n"
-
-        await message.channel.send(
-            content="" if member is None else member.mention,
-            embed=nextcord.Embed(
-                title=title, description=f"```{formatted_code}```", color=color
-            ),
-            reference=message,
-            allowed_mentions=nextcord.AllowedMentions(
-                replied_user=member is None, users=[member] if member else False
             ),
         )
 
@@ -302,14 +197,17 @@ class CodeRunner(Cog):
                 "restricted": restricted,
             }
         ).encode()
-        self.logger.debug(f"Running code:\n{code}")
+        formatted_user_input = "\n    ".join(user_input.split("\n"))
+        formatted_code = "\n    >>> ".join(code.split("\n"))
+        self.log.debug(
+            f"Running ({mode}, {restricted=}) code\n    >>> {formatted_code}\nInput\n    {formatted_user_input}"
+        )
         stdout, stderr = await proc.communicate(data)
         out, duration = self._split_run_time(stdout.decode())
 
-        self.logger.debug(f"Done {duration}\n{out}\n{stderr}\n{duration}")
         return out, stderr.decode(), duration
 
-    @Cog.command()
+    @command()
     async def eval(self, ctx, *, content):
         if content.casefold().strip() == "help":
             await ctx.send(
@@ -326,7 +224,7 @@ class CodeRunner(Cog):
 
         code = re.sub(r"^\s*(```(python|py)|`?)\s*|\s*(```|`)\s*$", "", content)
         title = "✅ Eval - Success"
-        color = BLUE
+        color = 0x0000FF
 
         code_message = f"\n```py\n>>> {code}"
 
@@ -335,7 +233,7 @@ class CodeRunner(Cog):
         output = out
         if err:
             title = "❌ Eval - Exception Raised"
-            color = YELLOW
+            color = 0xFFFF00
             output = err
 
         await ctx.send(
@@ -347,34 +245,7 @@ class CodeRunner(Cog):
             reference=ctx.message,
             mention_author=True,
         )
-
-    @Cog.command()
-    async def docs(self, ctx, *, content):
-        if not self.settings.get("EVAL_ENABLED", False):
-            await ctx.send("The docs command has been disabled.")
-            return
-
-        code = re.sub(r"^\s*(```(python|py)|`?)\s*|\s*(```|`)\s*$", "", content)
-        title = "Code Docs"
-        color = 0x4285F4
-
-        code_message = f"{ctx.author.mention} here are the docs you requested"
-        code_message += f"\n```py\n{code}```"
-
-        message, exceptions = await self.code_runner("docs", code)
-
-        if exceptions:
-            title = "Code Docs - Unable to retrieve"
-            color = 0xEA4335
-
-        output = "\n".join(message)
-        await ctx.send(
-            embed=nextcord.Embed(
-                title=title, description=f"```{output}\n```", color=color
-            ),
-            reference=ctx.message,
-            mention_author=True,
-        )
+        self.log.debug(f"{ctx.author} eval'd code from {ctx.message.jump_url}")
 
     def _split_run_time(self, content: str):
         parts = re.match(r"^(.+?)?\n\^{4}(\d+)\^{4}$", content, re.DOTALL)
